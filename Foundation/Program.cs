ea6d5973f6d94ca113b178f88815a90226a3fa22
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-10-04 15:18:30 -0600

    differnet ways to concat a string

---
+++ Foundation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace fdt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("whats your name uwu");
+            string strname = Console.ReadLine();
+            Console.WriteLine("hi " + strname + ". whats up");
+            Console.WriteLine("hello {0}, you cool", strname); // this is gross but sure
+            //Console.WriteLine("hoi ${} uwu");
+        }
+    }
+}
