c0d1d5627d272d0ac491d60e861c77715f633ebf
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-11-16 15:17:23 -0700

    notes on bindings

--- WPF/dataBinding.xaml.cs
+++ WPF/dataBinding.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,21 @@ namespace WPF
     /// </summary>
     public partial class dataBinding : Window
     {
+        ObservableCollection<Student> students = new ObservableCollection<Student>();
         public dataBinding()
         {
             InitializeComponent();
             DataContext = this;
+
+            // manual bind in c#
+            Binding binding = new Binding("Text");
+            binding.Source = txtvalue;
+            txtData.SetBinding(TextBlock.TextProperty, binding);
+
+            students.Add(new Student() { ID = 1, Name = "Carlos" });
+            students.Add(new Student() { ID = 2, Name = "Mark" });
+            students.Add(new Student() { ID = 3, Name = "Dan" });
+            lbStudents.ItemsSource = students;
         }
 
         private void updateSource_Click(object sender, RoutedEventArgs e)
@@ -30,5 +42,43 @@ namespace WPF
             BindingExpression be = txtTitle.GetBindingExpression(TextBox.TextProperty);
             be.UpdateSource();
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int newID = students[students.Count - 1].ID + 1;
+            students.Add(new Student() { ID = newID, Name = "Alph" });
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbStudents.SelectedItem != null)
+            { 
+                students.Remove((Student)lbStudents.SelectedItem);
+            }
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbStudents.SelectedItem != null)
+            {
+                // changing the list box is not the same as binding because it doesnt update for you
+                ((Student)lbStudents.SelectedItem).Name = "Sharpae";
+                lbStudents.Items.Refresh();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name = "";
+
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; } 
+        }
+
+
     }
 }
