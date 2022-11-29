1f11ef61e09f7fa22c9a8d016d53b5f9f00354b1
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-11-29 11:49:07 -0700

    sql linking

---
+++ WPF/db.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for db.xaml
+    /// </summary>
+    public partial class db : Window
+    {
+        public db()
+        {
+            InitializeComponent();
+        }
+
+        private void doit_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt;
+            Hashtable ht = new Hashtable();
+            string sql;
+            long lngReturn;
+
+            sql = "Select ID, Name from Names";
+            dt = ExDB.GetDataTable("CSC160", ht, sql);
+            dg.ItemsSource = dt.DefaultView;
+        }
+    }
+}
